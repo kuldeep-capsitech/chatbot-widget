0411bc2dc0baeaa3a0edb9594c520ba69900cb5b
biswarup-capsi <biswarup.naha@capsitech.com>
2025-08-19 19:53:17 +0530

    chat layout built

--- svelte-chat-widget/src/lib/Widget.svelte
+++ svelte-chat-widget/src/lib/Widget.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
-  import { fly, fade } from 'svelte/transition';
+  import { fly } from 'svelte/transition';
   import {open, openChat, openFaq} from "./store";
 
+  const handleOpen=() => {
+    open.update(val=>!val);
+    openChat.set(false);
+    openFaq.set(false);
+  };
+
   const handleOpenChat=()=>{
-    open.update(val=>!val)
+    if($open) open.update(val=>!val)
     openChat.update(val=>!val);
   }
-  // let open = $state(false);
+  const handleOpenFaq=()=>{
+    if($open) open.update(val=>!val); 
+    openFaq.update(val=>!val);
+
+  }
 </script>
 
 <style>
@@ -16,7 +26,7 @@
 <svelte:options customElement="chat-widget" />
 
 <div> 
-  <div class="chat-icon" aria-live="polite" aria-label="Chat" role="button" tabindex="0" onclick={() => open.update(val=>!val)} onkeydown={(e) => e.key === "Enter" && open.update(val=>!val)}>
+  <div class="chat-icon" aria-live="polite" aria-label="Chat" role="button" tabindex="0" onclick={handleOpen} onkeydown={(e) => e.key === "Enter" && handleOpen}>
   </div>
 
   {#if $open}
@@ -26,32 +36,41 @@
       <p>Nice to meet you! If you have any question about our services, feel free to contact us.</p>
     </div>
     <div class="welcome-2">
-      <div class="faq">FAQ</div>
+      <div class="faq" onclick={handleOpenFaq} onkeydown={e=>e.key==="Enter" && handleOpenFaq} aria-details="faq option" role="button" tabindex="0">FAQ</div>
       <div class="talk" onclick={handleOpenChat} onkeydown={e=>e.key==="Enter" && handleOpenChat} aria-details="chat option" role="button" tabindex="0">Let's Talk</div>
     </div>
       </div>
   {/if}
 
-  {#if $openFaq}
-      <div>hello</div>
+  {#if $openFaq && ($open==false)}
+      {open.set(false)}
+      <div>FAQs</div>
   {/if}
   
   {#if ($openChat && !($open))}
         <div class="chat-dialog" transition:fly="{{ x: 20, duration: 300 }}">
       <div class="chat-header">
-        <p>Chat Widget</p>
-        <div role="button" tabindex="0" onclick={() => openChat.update(val=>!val)} onkeydown={(e) => e.key === "Enter" && openChat.update(val=>!val)}>X</div>
+        <p>ChatFlow</p>
+        
+        <div role="button" tabindex="0" onclick={() => openChat.update(val=>!val)} onkeydown={(e) => e.key === "enter" && openChat.update(val=>!val)}>X</div>
       </div>
       <div class="chat-body">
-        Hello! How can I assist you today?
+        <p class="bot-msg">Hi! How Can I help You?</p>
         <div class="chat-options">
             <ul id="chat-options">
-                <li>select</li>
-                <li>About us</li>
-                <li>Conatct support</li>
+                <li>How do I apply?</li>
+                <li>What courses do you offer?</li>
+                <li> When do applications close?</li> 
+                <li>Where is the campus located?</li>
+                <li>Talk to someone?</li>
             </ul>
         </div>
-        <input type="text" placeholder="Enter your message here" />
+        
+         <div class="input">
+          <input type="text" placeholder="Let's share something" />
+          <!-- svelte-ignore a11y_consider_explicit_label -->
+          <button class="send">{">>"}</button>
+         </div>
       </div>
     </div>
   {/if}
