45f07d1a188a0e6b9970fda66e56715473ea3843
biswarup-capsi <biswarup.naha@capsitech.com>
2025-09-03 12:14:18 +0530

    http->https updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [preact(), basicSsl()],
+	server: {
+		https: true,
+		host: true // Optional: exposes to LAN
+	},
 	build: {
 		lib: {
 			entry: 'src/index.tsx',
@@ -30,3 +35,5 @@ export default defineConfig({
 		outDir: 'dist',
 	}
 });
+
+
